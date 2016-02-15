1396c8cc04c347c69d4fd7494d4ab85f98bc0853
dmedia <you@example.com>
2016-02-15 16:54:32 +0100

    osc

--- code/node/esempi/button.js
+++ code/node/esempi/button.js
@@ -1,43 +1,33 @@
 var five = require("johnny-five"),
     onButton7, onButton9, onButton8, ledRed, ledWhite, ledGreen;
 
-  var NetcatClient=require('node-netcat').client;
-client = require('node-netcat').client;
-var client = NetcatClient(4444, 'localhost');
+
+var osc = require('node-osc');
 
   five.Board().on("ready", function() {
-  onButton8 = new five.Button(8);
+  onButton8 = new five.Button(11);
   onButton9 = new five.Button(9);
-  onButton7 = new five.Button(7);
-  ledRed = new five.Led(2);
-  ledWhite = new five.Led(5);
-  ledGreen = new five.Led(3);
-  
+  onButton7 = new five.Button(10);
+  ledRed = new five.Led(3);
+  ledWhite = new five.Led(6);
+  ledGreen = new five.Led(5);
+  ledRed.off();
+  ledWhite.off();
+  ledGreen.off();
+
+
   console.log("Prima");
 		
-   onButton8.on("down", function(value){
-    client.start();
-
-   /*client.on('input', function () {
-   client.send('slideshow\n{"filename": "boy1.png", "transition": "blend1","duration": 3}\n', true);
-    client.close();  
-  });   */
-
-    /*client.on('open', function () {
-    
-    console.log('connect');
-    client.send('slideshow\n{"filename": "cat.png", "transition": "blend1","duration": 3}\n', true);
-    client.close(); 
-    });  
-	*/
-
-  client.on('data', function (data) {
-  console.log(data.toString('ascii'));
-  client.send('slideshow\n{"filename": "cat.png", "transition": "blend1","duration": 3}\n', true);
-    });
-    client.close();
+    onButton8.on("down", function(value){
     ledGreen.on();
-    console.log("Click 8");
+    console.log("Click 11");
+
+
+    var client = new osc.Client('156.148.132.240', 4444);
+    client.send('/photo/start/1', 200, function () {
+         console.log("send");
+         client.kill(); 
+        });
   });
 
  
@@ -50,7 +40,7 @@ var client = NetcatClient(4444, 'localhost');
 
     onButton7.on("down", function(value){
     ledRed.on();
-    console.log("Click 7");
+    console.log("Click 10");
   });
 
 });
