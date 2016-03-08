8eea8c5b18782d1a8a8b350e9aad6f37662c7511
mirella <you@example.com>
2016-03-08 23:23:05 +0100

    DC motor

--- code/node/esempi/DC_Motor/dc_motor.js
+++ code/node/esempi/DC_Motor/dc_motor.js
@@ -1,16 +1,51 @@
-var five = require("johnny-five"), 
-    board = new five.Board();
+var five = require("johnny-five"),
+  board, motor, led, pin;
 
-var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
-
-var motorA = new five.Motor(configs.A);
-var motorB = new five.Motor(configs.B);
+board = new five.Board();
 
 board.on("ready", function() {
+  // Create a new `motor` hardware instance.
+  motor = new five.Motor({
+    pin: 5
+  });
 
-  var motor = new five.Motor(5);
+  // Inject the `motor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    motor: motor
+  });
 
-  // Start the motor at maximum speed
-  motor.start(255);
+  
+  // Motor Event API
 
+  // "start" events fire when the motor is started.
+  motor.on("start", function(err) {
+
+    // Start the motor at maximum speed
+    motor.start(100);
+    console.log("start", Date.now());
+
+    // Demonstrate motor stop in 2 seconds
+    board.wait(2000, function() {
+      motor.stop();
+    });
+  });
+
+  // "stop" events fire when the motor is stopped.
+  motor.on("stop", function() {
+    console.log("stop", Date.now());
+  });
+
+  // Motor API
+
+  // start([speed)
+  // Start the motor. `isOn` property set to |true|
+  // Takes an optional parameter `speed` [0-255]
+  // to define the motor speed if a PWM Pin is
+  // used to connect the motor.
+  motor.start();
+
+  // stop()
+  // Stop the motor. `isOn` property set to |false|
 });
--- code/node/esempi/forceSensitive.js
+++ code/node/esempi/forceSensitive.js
@@ -1,5 +1,19 @@
 var five = require("johnny-five"),
-  fsr, led;
+  fsr, led, motor;
+
+/* http://node-ardx.org/exercises/14  */
+function getRGB(hue) {
+  var colors = [];
+  var r = five.Fn.constrain(five.Fn.map(hue, 0, 512, 255, 0), 0, 255);
+  var g = five.Fn.constrain(
+        five.Fn.map(hue, 0, 512, 0, 255), 0, 255) -
+      five.Fn.constrain(five.Fn.map(hue, 512, 1023, 0, 255),0,255);
+  var b = five.Fn.constrain(five.Fn.map(hue, 512, 1023, 0, 255), 0, 255);
+  colors[0] = r;
+  colors[1] = g;
+  colors[2] = b;
+  return colors;
+}
 
 (new five.Board()).on("ready", function() {
 
@@ -8,15 +22,36 @@ var five = require("johnny-five"),
       pin: "A0",
       freq: 25
     });
+    
+    led = new five.Led(3);
+    myRGBLed = new five.Led.RGB([ 9, 10, 11 ]);
 
-    led = new five.Led(9);
+    // Attach a motor to PWM pin 5
+    motor = new five.Motor({
+      pin: 5
+    });
+
+   // Attach a led to PWM pin 9
+   /* led = new five.Led({
+        pin: 9
+    }); */ 
 
     // Scale the sensor's value to the LED's brightness range
-    fsr.scale([0, 255]).on("data", function() {
-
+    fsr.scale([0, 255]).on("data", function(err, value) {
       // set the led's brightness based on force
       // applied to force sensitive resistor
-
       led.brightness(this.scaled);
+      
+     var rgbColors = getRGB(this.value);
+     myRGBLed.color(rgbColors);
+     //console.log("read value",this.value);
     });
+
+
+    /*fsr.on("read", function( err, value ) {
+    console.log("read value",this.value);
+    var rgbColors = getRGB(this.value);
+    myRGBLed.color(rgbColors);
+  	});   */
+
   });
